1dcaed104af8f0b3d7f9887621fc724c7e97e46a
c-keil <cwk.321@gmail.com>
2022-11-02 18:43:24 -0400

    added status publisher

--- avatar_imu_controller/scripts/arduino_driver.py
+++ avatar_imu_controller/scripts/arduino_driver.py
@@ -2,6 +2,7 @@
 
 import rospy
 from std_msgs.msg import Bool
+from std_msgs.msg import Int8
 import serial
 
 
@@ -9,17 +10,23 @@ import serial
 usb_string = '/dev/ttyACM0'
 threshold = 300 #no pressure is nominally 0, max reading is 1024
 
+WAITING = 0
+INITIALIZING = 1
+DRIVING = 2
+
 def status_pub():
     pub = rospy.Publisher('pressure_status', Bool, queue_size=10)
+    stat_pub = rospy.Publisher('driving_status', Int8, queue_size=10)
     rospy.init_node('pressure_sensor', anonymous=True)
     ser = serial.Serial(usb_string, 9600, timeout = 1)
     string = ser.readline()
     driving = False
     counter = 0
-    counter_thresh = 10
+    counter_thresh = 30
 
     while not rospy.is_shutdown():
         string = ser.readline()
+        stat_msg = Int8(WAITING)
 
         try:
             value = int(string[:-2])
@@ -30,14 +37,19 @@ def status_pub():
         if value > threshold:
             if not driving:
                 counter += 1
+                stat_msg = Int8(INITIALIZING)
             if counter > counter_thresh:
                 driving = True
+                stat_msg = Int8(DRIVING)
         else:
             driving = False
             counter = 0
+        #publish
         msg = Bool(driving)
-        # Bool.data = driving
         pub.publish(msg)
+        stat_pub.publish(stat_msg)
+
+        
 
 if __name__ == '__main__':
     try:
